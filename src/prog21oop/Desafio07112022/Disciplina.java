307a3061e9f65c8ef19a1fe86e94413a95a2e954
Tiago <ggtiago0santos@gmail.com>
2022-11-08 10:09:34 +0000

    yup

--- src/prog21oop/Desafio07112022/Disciplina.java
+++ src/prog21oop/Desafio07112022/Disciplina.java
@@ -1,5 +1,10 @@
 package prog21oop.Desafio07112022;
 
 public class Disciplina {
-    
+    public String nome;
+    public Professor p;
+
+    public void alocarProf(Professor p) {
+        this.p = p;
+    }
 }
--- src/prog21oop/Desafio07112022/Main.java
+++ src/prog21oop/Desafio07112022/Main.java
@@ -2,6 +2,6 @@ package prog21oop.Desafio07112022;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
