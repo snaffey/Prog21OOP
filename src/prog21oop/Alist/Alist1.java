a43d0f36abbd249c00d5663f9b138cabe5f92b90
Tiago <ggtiago0santos@gmail.com>
2022-11-11 09:37:47 +0000

    a

--- src/prog21oop/Alist/Alist1.java
+++ src/prog21oop/Alist/Alist1.java
@@ -67,5 +67,53 @@ public class Alist1 {
         boolean StIntCont = lIntCont.isEmpty();
         System.out.println("ArrayList vazio: " + StIntCont);
 
+        int ValorI1 = lIntCont.get(1);
+        int ValorI3 = lIntCont.get(3);
+        System.out.println("Valor da posição 1: " + ValorI1);
+        System.out.println("Valor da posição 3: " + ValorI3);
+
+        // Modificação
+        lIntCont.set(1, 18);
+        System.out.println(lIntCont);
+
+        // Exemplo 4
+        /*
+         * Remover um elemento de um ArrayList remove() - Remove um elemento de um
+         * índice num ArrayList
+         * removeAll() - Remove todos os elementos de um ArrayList
+         * removeIf() - Remove todos os elementos que correspondam a uma determinada
+         * entrada
+         * clear() - Remove todos os elementos de um ArrayList
+         */
+
+        List<String> lString = new ArrayList<>();
+        lString.add("PHP");
+        lString.add("Java");
+        lString.add("C#");
+        lString.add("Python");
+        lString.add("C++");
+        System.out.println(lString);
+        lString.remove(2);
+        System.out.println(lString);
+
+        // remoção por objetos, remove apenas o primeiro elemento encontrado
+        boolean isRem = lString.remove("PHP");
+        System.out.println(lString);
+
+        List<String> lString2 = new ArrayList<>();
+        lString2.add("PHP");
+        lString2.add("Java");
+        lString2.add("C#");
+        lString.removeAll(lString2);
+        System.out.println(lString2);
+        System.out.println(lString);
+
+        lString.removeIf(new Predicate<String>() {
+            @Override
+            public boolean test(String t) {
+                return t.startsWith("J");
+            }
+        });
+        System.out.println(lString);
     }
 }
